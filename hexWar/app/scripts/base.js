24828e9519e32347dd494b81d1c6e958b6c34f2e
Pal Szak <szak.pal@gmail.com>
2014-07-12 17:34:44 +0200

    Fix jshint errors

--- hexWar/app/scripts/base.js
+++ hexWar/app/scripts/base.js
@@ -1,36 +1,37 @@
+'use strict';
 // Install app
 if (navigator.mozApps) {
     var checkIfInstalled = navigator.mozApps.getSelf();
     checkIfInstalled.onsuccess = function () {
         if (checkIfInstalled.result) {
             // Already installed
-            var installationInstructions = document.querySelector("#installation-instructions");
+            var installationInstructions = document.querySelector('#installation-instructions');
             if (installationInstructions) {
-                installationInstructions.style.display = "none";
+                installationInstructions.style.display = 'none';
             }
         }
         else {
-            var install = document.querySelector("#install"),
-                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
-            install.className = "show-install";
+            var install = document.querySelector('#install'),
+                manifestURL = location.href.substring(0, location.href.lastIndexOf('/')) + '/manifest.webapp';
+            install.className = 'show-install';
             install.onclick = function () {
                 var installApp = navigator.mozApps.install(manifestURL);
                 installApp.onsuccess = function(data) {
-                    install.style.display = "none";
+                    install.style.display = 'none';
                 };
                 installApp.onerror = function() {
-                    alert("Install failed\n\n:" + installApp.error.name);
+                    alert('Install failed\n\n:' + installApp.error.name);
                 };
             };
         }
     };
 }
 else {
-    console.log("Open Web Apps not supported");
+    console.log('Open Web Apps not supported');
 }
 
 // Reload content
-var reload = document.querySelector("#reload");
+var reload = document.querySelector('#reload');
 if (reload) {
     reload.onclick = function () {
         location.reload(true);
